d7ff05080fe5710440f355373a8230baf7bd0801
DavidNNussbaum <brocknynj@aol.com>
2020-11-02 17:44:53 -0500

    commit tree 5aad3925c00322b963e164cfdc0f53ef46c5b645

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,28 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    if  word.match(/^[aeiouAEIOU]\w+/)
+        return true
+    else
+        false
+    
+    end
+        
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    holder = []
+    i = 0
+    ar_words = text.split
+    ar_words.select do |item|
+        item.match(/^un\w+.ing$/)
+        #  while i < ar_words.length
+            # if item.match(/^un\w+.ing$/)
+            # holder << item
+            # i += 1
+            # end
+        #  end
+    end
+    # return               
 end
 
 def words_five_letters_long(text)
